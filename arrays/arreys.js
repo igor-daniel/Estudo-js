daed043020f97c7ec8f91a42af7e528168cd26c3
igor-daniel <igord1870@gmail.com>
2019-02-04 22:00:02 -0200

    Array adicionado

---
+++ arrays/arreys.js
@@ -0,0 +1,15 @@
+let produtos = ["blusa", "shorts", "camiseta", "tenis",] 
+
+let clientes = ["João", "José", "Marcos"] //para criar um array.
+
+produtos.pop() //Este código retira o último elemento do array.
+
+delete clientes[1] //para deletar um elemento do array.
+console.log(clientes)
+console.log(produtos)
+
+clientes.push("Ronaldo", "Alex" ) //Para adicionar elementos no array.
+console.log(clientes)
+
+console.log(produtos.length) //Para verificar a quantidade de elementos no array.
+console.log(typeof clientes)
