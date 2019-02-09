a75f0ceee94090e3852685b126915da4d258bae5
igor-daniel <igord1870@gmail.com>
2019-02-09 18:27:16 -0200

    let e var em loops adicionados.

---
+++ VareLet/UsandoLetemLoops.js
@@ -0,0 +1,7 @@
+for (let n = 0; n < 10; n++) {
+    console.log(n)
+}
+
+//console.log(n) 
+
+//Gera erro pois let é visível apena dentro do bloco.
---
+++ VareLet/UsandoVaremLoops.js
@@ -0,0 +1,11 @@
+for (i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log("i =" + i)
+
+/* O for é usado para criar loops, para usar for é preciso dar 3 informações.
+ *A primeira é criar uma variável.
+ *A segunda é fazer uma comparação.
+ *E a terceira, neste caso é adicionar mais um.
+ */
