43e7a0bd6e654ef7d18652c5e55fdfd07c0e5f34
igor-daniel <igord1870@gmail.com>
2019-02-11 16:58:07 -0200

    Operadores de Atribuição adicionados.

---
+++ numeros/OperadoresDeAtribuição.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 3
+
+b += a  // Mesma coisa que: b = b + a  Adição
+console.log(b)
+
+b -= a  // b = b - a  Subtração
+console.log(b)
+
+b *= a  // b = b * a  Multiplicação
+console.log(b)
+
+b /= a // b = b / a  Divisão
+console.log(b)
+
+b %= a // b = b % a  Modulo.  Que é o resto da divisão entre os números.
