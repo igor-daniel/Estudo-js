dd40dd203305e0a968f5180d1025a09f6fd0cc8e
igor-daniel <igord1870@gmail.com>
2019-02-16 23:26:14 -0300

    Operadores Aritimeticos

---
+++ numeros/OperadoresAritiméticos.js
@@ -0,0 +1,11 @@
+var [a, b, c, d] = [3, 8, 10, 4]  //Forma compacta de criar várias variáveis.
+
+//Operadores binários.
+var soma = a + b + c + d;
+var subtracao = c - d;
+var multiplicacao = a * d;
+var divisao = a / c;
+var modulo = a % 2; // Modulo é o resto da divisão.
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
+console.log(-soma); //Operador unário.
