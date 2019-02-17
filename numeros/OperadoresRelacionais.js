65d5b7dfe63a980a47d725ec90c0864e1f132f17
igor-daniel <igord1870@gmail.com>
2019-02-16 23:48:35 -0300

    Operadores Relacionais adicionados.

---
+++ numeros/OperadoresRelacionais.js
@@ -0,0 +1,17 @@
+console.log("01)", "1" == 1) //Aqui está sendo comparado apenas o valor.
+console.log("02)", "1" === 1) //Já aqui está sendo comparado o valor e o tipo.
+console.log("03)", "3" != 4) // != quer dizer diferente.
+console.log("04)", "3" !== 3) //Está sendo comparado o valor e o tipo.
+
+console.log("05)", 5 < 4)
+console.log("06)", 5 > 4)
+console.log("07)", 5 <= 5)
+console.log("08)", 8 >= 5)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log("09)", d1 == d2)
+console.log("10)", d1 === d2)
+
+console.log("11)", undefined == null)
+console.log("12)", undefined === null)
