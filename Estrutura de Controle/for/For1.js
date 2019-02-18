f6a2801cc1f6c374bcd44bee166ec7eda812d811
igor-daniel <igord1870@gmail.com>
2019-02-18 19:36:31 -0300

    For 1 adicionado.

---
+++ Estrutura de Controle/for/For1.js
@@ -0,0 +1,19 @@
+let contador = 1 
+while (contador <= 10) {
+    console.log(`Loading ${contador}`)
+    contador++
+}
+
+//Criando o mesmo exemplo de cima, só que com for.
+
+console.log(" ")
+//Na estrutura for primeiro tem que ser criada uma variável uma condição, e por ultimo pedir para executar algo.
+for (let i = 1; i <= 10; i++) {
+    console.log(`Carregando... ${i}`)
+}
+
+
+const notas = [8.74, 9.65, 2.36, 6.54, 7.45]
+for (let i = 0; i < notas.length; i++){
+    console.log(`Notas do bimestre... ${notas[i]}`)
+}
